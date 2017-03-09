b2c883d1e906562e86fb6ab30ba5f44522be05a4
Moses Aronov <moses@aronov.me>
2017-03-09 11:04:57 -0500

    Added addTask Function

--- src/app.component.ts
+++ src/app.component.ts
@@ -27,4 +27,10 @@ export class AppComponent {
         )
     ]
     private currentTask = new Task(null, false)
+
+    addTask(){
+        let task = new Task(this.currentTask.content, this.currentTask.completed);
+        this.tasks.push(task)
+
+    }
 }
